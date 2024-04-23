4fc30b911d4a03285bcc380da36cc8490b2076bf
Jelena Nikolic <nikolic_jelena@outlook.com>
2024-04-24 00:21:43 +0200

    s3 bucket

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,18 +9,8 @@ export class AppController {
   constructor(private appService: AppService) {}
 
   @Post()
-  @UseInterceptors( FilesInterceptor('files[]', 5, {
-    storage: diskStorage({
-      destination: './uploads',
-      filename: (req, file, callback) => {
-        const suffix = Date.now() + '-';
-        const extension = extname(file.originalname);
-        callback(null, `${file.fieldname}-${suffix}${extension}`);
-      },
-    }),
-  }),)
+  @UseInterceptors( FilesInterceptor('files[]', 5,))
   uploadImage(@UploadedFiles() files:any): any {
-    console.log(files)
     this.appService.uploadImage(files)
   }
 
--- src/app.service.ts
+++ src/app.service.ts
@@ -9,18 +9,47 @@ const writeFileAsync = promisify(fs.writeFile);
 
 @Injectable()
 export class AppService {
+  AWS_S3_BUCKET = 'im-homework';
+  s3 = new AWS.S3({
+    apiVersion:"2006-03-01",
+    region: "us-east-1",
   
-  async uploadImage(files: any) {
-    console.log(files)
+  });
 
-    // if (files.length > 1) { 
-    //   for (let file of files) {
-    //     const filePath = `./uploads/${file.originalname}`;
-    //     await writeFileAsync(filePath, Buffer.from(file, "base64"));
-    //   }
-    // } else {
-    //   const filePath = `./uploads/${files[0].originalname}`;
-    //   await writeFileAsync(filePath, Buffer.from(files[0], "base64"));
-    // }
+  async uploadImage(files) {
+    console.log(files);
+    if (files.length < 0) {
+      return Error
+    }
+    let file = files[0]
+    const { originalname } = file;
+
+    return await this.s3_upload(
+      file.buffer,
+      this.AWS_S3_BUCKET,
+      originalname,
+      file.mimetype,
+    );
   }
+
+  async s3_upload(file, bucket, name, mimetype) {
+    const params = {
+      Bucket: bucket,
+      Key: String(name),
+      Body: file,
+      ACL: 'public-read',
+      ContentType: mimetype,
+      ContentDisposition: 'inline',
+    };
+
+    try {
+      let s3Response = await this.s3.upload(params).promise();
+      return s3Response;
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  // async uploadImage(files: any) {
+  // }
 }
