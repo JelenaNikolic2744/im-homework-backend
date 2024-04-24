029543e77420cf453cc4250e2a79a1fc5db48a11
Jelena Nikolic <nikolic_jelena@outlook.com>
2024-04-24 11:49:24 +0200

    uploadMultipleS3

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { error } from 'console';
 
 let size = 0
 
@@ -8,17 +9,17 @@ let size = 0
 export class AppService {
   AWS_S3_BUCKET = 'im-homework';
 
-  constructor(private configService: ConfigService){}
+  constructor(private configService: ConfigService) { }
 
-  getSize(){
+  getSize() {
     return size
   }
 
-  countSize(files){
-    if(files.length === 1){
+  countSize(files) {
+    if (files.length === 1) {
       size += files[0].size
     }
-    else{
+    else {
       files.forEach(file => {
         size += file.size
       });
@@ -34,22 +35,24 @@ export class AppService {
   });
   async uploadImage(files) {
     this.countSize(files)
-    console.log(files);
     if (files.length === 0) {
       return Error;
     }
-    let file = files[0];
-    const { originalname } = file;
+    let uploadedImages = []
+    if (files.length > 1) {
+      files.forEach((file) => {
+        uploadedImages.push(this.s3_upload(
+          file.buffer,
+          this.AWS_S3_BUCKET,
+          file.originalname,
+          file.mimetype,
+        ))
+      })
+    }
+    Promise.all(uploadedImages).then((value) =>{console.log(value)}).catch((error)=>console.log(error))
+    console.log(uploadedImages)
 
-    let temp = await this.s3_upload(
-      file.buffer,
-      this.AWS_S3_BUCKET,
-      originalname,
-      file.mimetype,
-    );
-    console.log(temp)
-
-    return temp
+    return uploadedImages
   }
 
   async s3_upload(file, bucket, name, mimetype) {
